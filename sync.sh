d3142bf948014870e61295720a040479a8e42b1d
James Lu <GLolol@overdrivenetworks.com>
2016-12-21 16:21:52 -0800

    packagelists: remove outdated sync.sh

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUTDIR="packages.overdrivenetworks.com.mirror"
-
-rsync --verbose -a --progress --bwlimit=10100 pub@45.79.66.40::packages "$OUTDIR" --delete-after --exclude .lastsync --password-file ~/.rsync.pwd
-
-echo ""
-echo "Writing last synced time to $OUTDIR/.lastsync"
-date -Ru | tee $OUTDIR/.lastsync
