0e2540585b44f0770709148505670a54c49be785
James Lu <james@overdrivenetworks.com>
2018-08-25 03:15:00 +0000

    autobuilder: resync run.sh config

--- autobuilder/run.sh
+++ autobuilder/run.sh
@@ -8,13 +8,16 @@ source "config.sh"
 echo 'Sourcing buildercore.sh'
 source "buildercore.sh"
 
-# Repositories to build: customize everything under this line!
+# Repositories to build. Args to build_git:
+#    package name (a subfolder in the autobuilder folder),
+#    upstream branch to merge (will run git merge $GITBUILDER_UPSTREAM_REMOTE/<this field>)
+#    branch containing packaging (the branch to merge into)
 build_git "youtube-dl" "master" "master"
+build_git "you-get" "develop" "utopia-nightly"
 
 publish
 sleep 3
 
-cd "$CURDIR"  # I don't know why this line is needed...
 echo 'Sourcing config-stretch.sh'
 source "config-stretch.sh"
 
