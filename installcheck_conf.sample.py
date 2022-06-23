79c876e0a29bf66a16eeaa73264884a5b09cafdb
James Lu <james@overdrivenetworks.com>
2022-06-23 02:11:25 -0700

    Refresh installcheck config for bullseye

--- installcheck_conf.sample.py
+++ installcheck_conf.sample.py
@@ -1,8 +1,6 @@
 # A list of known repositories.
-REPOS = {'debian': 'http://httpredir.debian.org/debian',
-         'urepo': 'https://deb.utopia-repository.org',
-         'urepo-nightlies': 'https://ni.deb.utopia-repository.org',
-         'ubuntu': 'http://archive.ubuntu.com/ubuntu'}
+REPOS = {'debian': 'http://deb.debian.org/debian',
+         'urepo': 'https://deb.utopia-repository.org',}
 
 # A list of architectures to test.
 TARGET_ARCHS = ['amd64', 'i386']
@@ -12,31 +10,27 @@ TARGET_ARCHS = ['amd64', 'i386']
 # e.g. Utopia Repository targets declare either Debian main or Ubuntu main+universe in their dependencies.
 TARGET_DISTS = {
     ('urepo', 'sid', 'main'): [('debian', 'sid', 'main')],
+    ('urepo', 'sid', 'meta'): [('debian', 'sid', 'main'), ('urepo', 'sid', 'main')],
     ('urepo', 'sid', 'imports'): [('debian', 'sid', 'main'),
                                   ('debian', 'sid', 'contrib'),
                                   ('debian', 'sid', 'non-free'),
                                   ('urepo', 'sid', 'main')],
-    ('urepo', 'sid', 'forks'): [('debian', 'sid', 'main'),
-                                ('urepo', 'sid', 'main')],
-    ('urepo-nightlies', 'sid-nightlies', 'main'): [('debian', 'sid', 'main'),
-                                                   ('urepo', 'sid', 'main')],
 
-    ('urepo', 'stretch', 'main'): [('debian', 'stretch', 'main'),
-                                   ('debian', 'stretch-backports', 'main'),
-                                   ('urepo', 'stretch', 'imports'),
+    ('urepo', 'bullseye', 'main'): [('debian', 'bullseye', 'main'),
+                                   ('debian', 'bullseye-backports', 'main'),
+                                 ],
+    ('urepo', 'bullseye', 'meta'): [('debian', 'bullseye', 'main'),
+                                    ('debian', 'bullseye-backports', 'main'),
+                                    ('urepo', 'bullseye', 'main'),
                                   ],
-    ('urepo', 'stretch', 'imports'): [('debian', 'stretch', 'main'),
-                                      ('debian', 'stretch', 'contrib'),
-                                      ('debian', 'stretch', 'non-free'),
-                                      ('urepo', 'stretch', 'main'),
-                                     ],
-    ('urepo', 'stretch', 'forks'): [('debian', 'stretch', 'main'),
-                                    ('urepo', 'stretch', 'main'),
-                                   ],
+    ('urepo', 'bullseye', 'imports'): [('debian', 'bullseye', 'main'),
+                                      ('debian', 'bullseye', 'contrib'),
+                                      ('debian', 'bullseye', 'non-free'),
+                                      ('urepo', 'bullseye', 'main'),
+                                  ],
 
     ('urepo', 'buster', 'main'): [('debian', 'buster', 'main'),
                                    ('debian', 'buster-backports', 'main'),
-                                   ('urepo', 'buster', 'imports'),
                                  ],
     ('urepo', 'buster', 'imports'): [('debian', 'buster', 'main'),
                                       ('debian', 'buster', 'contrib'),
@@ -46,18 +40,4 @@ TARGET_DISTS = {
     ('urepo', 'buster', 'forks'): [('debian', 'buster', 'main'),
                                     ('urepo', 'buster', 'main'),
                                   ],
-
-    ('urepo', 'xenial', 'main'): [('ubuntu', 'xenial', 'main'),
-                                  ('ubuntu', 'xenial', 'universe')],
-
-    ('urepo', 'bionic', 'main'): [('ubuntu', 'bionic', 'main'),
-                                  ('ubuntu', 'bionic', 'universe')],
-    ('urepo', 'bionic', 'imports'): [('ubuntu', 'bionic', 'main'),
-                                      ('ubuntu', 'bionic', 'universe'),
-                                      ('ubuntu', 'bionic', 'restricted'),
-                                      ('ubuntu', 'bionic', 'multiverse'),
-                                      ('urepo', 'bionic', 'main')],
-    ('urepo', 'bionic', 'forks'): [('ubuntu', 'bionic', 'main'),
-                                   ('ubuntu', 'bionic', 'universe'),
-                                   ('urepo', 'bionic', 'main')],
 }
