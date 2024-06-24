451c6759163fc2e14bdb0a9dd16f3e301332619f
James Lu <james@overdrivenetworks.com>
2024-06-23 22:43:09 -0700

    Add newupstream.sh, release.sh

---
+++ newupstream.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$1" ]]; then
+	echo "usage: $0 <new package version>"
+    echo "Bump a Debian package's changelog version and commit the change to git"
+	exit 1
+fi
+
+ver="$1"
+shift
+set -x
+dch -v "$ver" -m "New upstream release." "$@"
+git commit debian/changelog -m "Bump changelog to $ver"
---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+curr_dist="$(dpkg-parsechangelog -S Distribution)"
+ver="$(dpkg-parsechangelog -S Version)"
+if [[ "$curr_dist" != "UNRELEASED" ]]; then
+	echo "Current distribution is $curr_dist, not UNRELEASED"
+	# show current changelog entry for convenience
+	dpkg-parsechangelog -S Changes
+	exit 1
+fi
+
+dist="${1:-unstable}"
+set -x
+dch -r --no-force-save-on-release --distribution "$dist"
+git commit debian/changelog -m "Release $ver to $dist"
+if [[ -z "$NOTAG" ]]; then
+	git tag -s "debian/$ver" -m "Debian release $ver"
+fi
