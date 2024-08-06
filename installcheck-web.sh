aeb2601461556fec107269455c40f16587cd17b4
James Lu <james@overdrivenetworks.com>
2024-08-05 21:41:54 -0700

    installcheck-web: refactor to not depend on autoindexing
    This is not supported on Cloudflare R2

--- installcheck-web.sh
+++ installcheck-web.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Run installcheck regularly, saving the results to the website and reporting checker status to a webhook.
+shopt -s nullglob
 
 DATE_PART="$(date +%Y%m%d-%s)"
 OUTPUT_DIR="/srv/aptly-web/installcheck/${DATE_PART}"
-#OUTPUT_DIR="/tmp/installcheck/${DATE_PART}"
-URL="https://deb-master.utopia-repository.org/installcheck/${DATE_PART}"
+URL_BASE="https://deb-master.utopia-repository.org/installcheck/${DATE_PART}"
 
 script_dir="$(realpath "${0%/*}")"
 
@@ -14,18 +14,19 @@ cd "$OUTPUT_DIR" || exit 1
 # TODO: this should report if some Packages list failed to download
 "$script_dir"/installcheck.py
 
-failed_count=$(find . -maxdepth 1 -type f | wc -l)
-if [[ failed_count -ge 0 ]]; then
-    echo "The following dists failed installability checks:"
-    ls -l
-    webhook_text="${failed_count} installability check(s) failed: ${URL}"
-else
+webhook_text=""
+for report in *.txt; do
+    webhook_text+=$"Installability check failed: ${URL_BASE}/${report}\n"
+done
+if [[ -z "$webhook_text" ]]; then
     webhook_text="All installability checks passed"
-    echo "$webhook_text"
 fi
+
+echo -e "$webhook_text"
 
 if [[ -n "$WEBHOOK_URL" ]]; then
     echo "Sending webhook announcement..."
+    echo -n "Webhook response: "
     curl -d "{\"text\":\"$webhook_text\"}" -H 'Content-Type: application/json' \
         "$WEBHOOK_URL"
 fi
